905129575527454cfa68ecb941da94c585bd81a3
hector <hectorgarcia849@gmail.com>
2017-10-18 16:12:37 -0400

    Add index.js file as module access point

---
+++ index.js
@@ -0,0 +1,6 @@
+const {User} = require('./models/user');
+const {Pigeon} = require('./models/pigeon');
+const {Profile} = require('./models/profile');
+const {Chat} = require('./models/chat');
+
+module.exports = {User, Pigeon, Profile, Chat}
